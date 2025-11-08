8a5a981c2dfe6455f4184cde98221c988df3fa21
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-11-08 16:16:19 +0600

    First Setup of Planora along with AUthJS v5

---
+++ auth.ts
@@ -0,0 +1,51 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { getUserFromDb, saltAndHashPassword } from "./lib/utils"
+import { signInSchema } from "./lib/zod"
+import { ZodError } from "zod"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Credentials({
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "password",
+        },
+      },
+      authorize: async (credentials) => {
+        try {
+            let user = null
+
+
+        const { email, password } = await signInSchema.parseAsync(credentials)
+ 
+        // logic to salt and hash password
+        const pwHash = saltAndHashPassword(password as string)
+ 
+        // logic to verify if the user exists
+        user = await getUserFromDb(email as string, pwHash)
+ 
+        if (!user) {
+          // No user found, so this is their first attempt to login
+          // Optionally, this is also the place you could do a user registration
+          throw new Error("Invalid credentials.")
+        }
+ 
+        // return user object with their profile data
+        return user
+        }catch (error) {
+          if (error instanceof ZodError) {
+            // Return `null` to indicate that the credentials are invalid
+            return null
+          }
+          return null
+        }
+      }, 
+    }),],
+})
---
+++ lib/utils.ts
@@ -0,0 +1,20 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+
+export function saltAndHashPassword(password: string) {
+  return password;
+}
+
+
+
+export function getUserFromDb(email: string, password: string) {
+  return {
+    email,
+    password,
+  }
+}
