48002fd6f3315e43b6bcaa4b9c21e5727747d21b
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-11-09 22:03:10 +0600

    added the authn form

---
+++ components/ui/AuthenticationDialog.tsx
@@ -0,0 +1,119 @@
+"use client";
+import * as z from "zod";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "./input";
+import { Button } from "./button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "./form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Field, FieldDescription } from "./field";
+import { signInAction } from "@/app/actions/auth";
+import { useState } from "react";
+
+
+
+const formSchema = z.object({
+    email: z.string().email({ message: "Invalid email address." }),
+    password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters." }),
+});
+
+export function AuthenticationDialog() {
+
+
+
+    const [isSignInForm, setIsSignInForm] = useState(false);
+
+
+
+
+    // ✅ Initialize form correctly
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+    },
+  });
+  
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+      await signInAction(values);
+    };
+    
+    
+    const handleFormSwitch = () => {
+      setIsSignInForm(!isSignInForm);
+    };
+
+  return (
+    <Dialog>
+      <DialogTrigger className="cursor-pointer">Open</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Sign In</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            className="grid gap-6 py-4"
+            onSubmit={form.handleSubmit(onSubmit)}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem className="grid gap-3">
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input type="email" placeholder="john@example.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="grid gap-3">
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Your Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter className="mt-4">
+              <Field>
+                <Button type="submit">Login</Button>
+                <Button variant="outline" type="button">
+                  Login with Google
+                </Button>
+                <FieldDescription className="text-center">
+                  Don&apos;t have an account? <a onClick={handleFormSwitch} href="#">{isSignInForm ? "Sign in" : "Sign up"}</a>
+                </FieldDescription>
+              </Field>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
