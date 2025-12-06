96b019efc2f70e4012c4c6af157b3c37e337d52e
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2025-12-06 21:21:38 +0600

    added all the rtk apis to the store

--- store/store.ts
+++ store/store.ts
@@ -1,15 +1,24 @@
+import subtaskApi from '@/api/subtaskApi';
+import taskApi from '@/api/taskApi';
 import userApi from '@/api/userApi';
+import workspaceApi from '@/api/workspaceApi';
 import { configureStore } from '@reduxjs/toolkit';
 
 export const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
+    [taskApi.reducerPath]: taskApi.reducer,
+    [workspaceApi.reducerPath]: workspaceApi.reducer,
+    [subtaskApi.reducerPath]: subtaskApi.reducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }).concat(
-      userApi.middleware
+      userApi.middleware,
+      taskApi.middleware,
+      workspaceApi.middleware,
+      subtaskApi.middleware
     ),
 });
 
